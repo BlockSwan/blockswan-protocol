6d7c707d003f49f075c508cf7ea08e937a6422ec
Oscar Macieira <macieiraoscar@icloud.com>
2022-12-29 14:38:48 +0100

    Updating config files for deployment and untracking unnecessary files

--- backend/src/config/node.ts
+++ backend/src/config/node.ts
@@ -1,13 +1,14 @@
 import { Options } from 'ipfs-core/dist/src/types'
+import { IP } from '../utils/envs'
 
 const nodeConfig: Options = {
 	repo: './ipfs/',
 
 	config: {
 		Addresses: {
-			Swarm: ['/ip4/172.20.10.3/tcp/4001'],
+			Swarm: [`/ip4/${ IP }/tcp/4001`],
 			Gateway: '/ip4/0.0.0.0/tcp/8080',
-			API: '/ip4/172.20.10.3/tcp/5001',
+			API: `/ip4/${ IP }/tcp/5001`,
 		},
 		Bootstrap: [],
 	},
--- backend/src/databaseConnection.ts
+++ backend/src/databaseConnection.ts
@@ -4,8 +4,8 @@ import { MONGODB_URI } from './utils/envs'
 mongoose.Promise = global.Promise
 
 const connectToDatabase = async (): Promise<void> => {
-    //const options: ConnectionOptions = { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true };
-    await mongoose.connect(MONGODB_URI) //add option
+	//const options: ConnectionOptions = { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true };
+	await mongoose.connect(MONGODB_URI) //add option
 }
 
 export { connectToDatabase }
